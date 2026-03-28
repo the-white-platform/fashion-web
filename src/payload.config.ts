7a5f18f28b1fe721c6a3cc8b86ddeb5ea681c750
kanetran29 <kanetran29@gmail.com>
2026-03-28 21:41:44 +0200

    refactor: move admin components to src/admin/

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -56,14 +56,14 @@ export default buildConfig({
     components: {
       // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
-      beforeLogin: ['@/components/BeforeLogin'],
+      beforeLogin: ['@/admin/BeforeLogin'],
       // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
       // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
-      beforeDashboard: ['@/components/BeforeDashboard'],
-      afterNavLinks: ['@/components/AccountingLink'],
+      beforeDashboard: ['@/admin/BeforeDashboard'],
+      afterNavLinks: ['@/admin/AccountingLink'],
       views: {
         Accounting: {
-          Component: '@/components/AccountingView',
+          Component: '@/admin/AccountingView',
           path: '/accounting',
         },
       },
